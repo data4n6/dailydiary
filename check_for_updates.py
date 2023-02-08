9e9e2a208cdfe44c93c3279e7b84c795709c4c97
data4n6s <92990133+data4n6s@users.noreply.github.com>
2023-02-08 07:25:25 -0700

    Working on check for updates logic

--- check_for_updates.py
+++ check_for_updates.py
@@ -45,7 +45,7 @@ def isUpToDate(fileName,url):
 
 def update_files(fileName, url):
     for i in tqdm(range(1), desc=f"Downloading Updates for {fileName}..."):
-        urllib.request.urlretrieve(url, path)
+        urllib.request.urlretrieve(fileName, url)
 
 
 #def checkForUpdates(fileName):
