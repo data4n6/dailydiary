50241007bd07ae0bd8817387816ed2f7c842f50d
data4n6s <92990133+data4n6s@users.noreply.github.com>
2023-02-08 20:41:36 -0700

    Modified: check_for_updates.py - Added all new logic, complete rebuild

--- check_for_updates.py
+++ check_for_updates.py
@@ -1,58 +1,89 @@
 import requests
-import hashlib
-import urllib.request
-from tqdm import tqdm
+from github import Github
 import os
+import shutil
+import urllib.request 
+from config import token, username, url, repo
+from tqdm import tqdm
 
-# Steps:
-# Check is this file current
-# Then check if all other files are current
-# Check if any files are deleted
-# Check if there are any new files
 
-def update():
-    ''' Starts the update process'''
 
-    url = "https://raw.githubusercontent.com/data4n6s/dailydiary/main/"
-    print("Checking for updates...")
-    if update_me(url) == True:
-        update()
-    else:
-        print("First Step Complete...")
+version = "v1.0.1"
+g = Github(token)
+user_data = requests.get(url, auth=(username,token)).json()
+tag = user_data[0]["tag_name"]
+repository = g.get_user(username).get_repo(repo)
+local_dir = "python-files"
 
-def update_me(url):
-    ''' Checks if the Update file: check_for_updates.py has any updates'''
+# About
+print("="*50)
+print("Full name:", repository.full_name)
+print("Description:", repository.description)
+print("Date created:", repository.created_at)
+print("Date of last push:", repository.pushed_at)
+print("Home Page:", repository.homepage)
+print("Language:", repository.language)
+print("Number of forks:", repository.forks)
+print("Number of stars:", repository.stargazers_count)
+print("-"*50)
 
-    file = os.path.basename(__file__)
-    if isUpToDate(__file__, url+file) == False:
-        print("Updates found...")
-        update_files(__file__, url+file,file)
+def does_path_exists(path):
+    ''' Checks to see if the path exists'''
+    
+    if os.path.exists(path):
+        print(f"Yes {path} exists, sending True")
         return True
     else:
+        print(f"No {path} does not exist, sending False")
         return False
 
-def isUpToDate(fileName,url):
+def update_file(url, filenamepath):
+    for i in tqdm(range(1), desc=f"Downloading Updates for {filenamepath}..."):
+        urllib.request.urlretrieve(url, filenamepath)
+        
+        
+def download_directory(repository, local_dir,delete_dir=True,remote_dir=""):
+    """
+    Download all contents from github with latest tag from repository.
+    """
+    print(f"Delete Dir: {delete_dir}")
+    if delete_dir:
+        try:
+            if does_path_exists(local_dir):
+                shutil.rmtree(local_dir)
+                print(f"{local_dir} deleted.")
+        except:
+            pass
 
-    with open(fileName, "r") as f:
-        file = f.read()
-    hash = hashlib.sha256((file).encode('utf-8')).hexdigest()
-    urlcode = requests.get(url).text
-    urlhash = hashlib.sha256((urlcode).encode('utf-8')).hexdigest()
-    if hash == urlhash:
-        return True
+        finally: 
+            os.makedirs(local_dir)
+            print(f"{local_dir} created.")            
+
+    elif does_path_exists(local_dir):
+        print(f"Path: {local_dir} exists. Do Nothing")
+        pass
     else:
-        return False
+        os.makedirs(local_dir)
+        print(f"{local_dir} created.") 
 
-def update_files(fileName, url, file):
-    for i in tqdm(range(1), desc=f"Downloading Updates for {file}..."):
-        urllib.request.urlretrieve(url, fileName)
+    for content in repository.get_contents(remote_dir):
+        if content.type == "dir":
+            path = os.path.join(local_dir,content.path)
+            print(f"Found Dir: {content.path}")
+            print(f"Need to Create Path: {path}")
+            if does_path_exists(path):
+                print(f"Path: {path} exists. Do Nothing")
+                pass
+            else:
+                os.makedirs(path)
+                print(f"{path} created.") 
+            download_directory(repository,local_dir,False,content.path)
+        else:
+            filenamepath = os.path.join("python-files",content.path)
+            update_file(content.download_url, filenamepath)
+            
 
-
-#def checkForUpdates(fileName):
-#
-#
-#    if isUpToDate(fileName, url) == False:
-#        update(path, url)
-#        return True
-#    else:
-#        return False
+if tag > version:
+    download_directory(repository, local_dir)
+else:
+    pass
